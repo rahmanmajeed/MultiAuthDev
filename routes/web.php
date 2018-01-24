ed42cc2080babf6a79b39188b6190721594f4b96
Majeed <rahmanmajeed46@gmail.com>
2018-01-25 00:46:58 +0600

    Controller,Guards,view Setup

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->group(function(){
+
+Route::get('/', 'AdminController@index')->name('admin.dashboard');
+
+Route::get('/login','Auth\AdminLoginController@showLoginForm')->name('admin.login');
+
+Route::post('/login','Auth\AdminLoginController@login')->name('admin.login.submit');
+
+});
+
